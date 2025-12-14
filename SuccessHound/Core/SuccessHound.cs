581d61e3f5e3527d448566b335a5159487177adc
Cody <cydoentis@gmail.com>
2025-12-14 15:02:57 -0500

    feat: add SuccessHound core class and default ApiResponse factory

--- SuccessHound/Core/SuccessHound.cs
+++ SuccessHound/Core/SuccessHound.cs
@@ -1,6 +1,27 @@
-﻿namespace SuccessHound.Core;
+﻿using SuccessHound.Abstractions;
 
-public class SuccessHound
+namespace SuccessHound.Core;
+
+public static class SuccessHound
 {
-    
+    private static ISuccessResponseFactory? _factory;
+
+    /// <summary>
+    /// Configures SuccessHound with a user-provided response factory.
+    /// </summary>
+    public static void Configure(ISuccessResponseFactory factory)
+    {
+        _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+    }
+
+    /// <summary>
+    /// Wraps raw data using the configured factory.
+    /// </summary>
+    internal static object Wrap(object data)
+    {
+        if (_factory == null)
+            throw new InvalidOperationException("SuccessHound is not configured. Call SuccessHound.Configure() first.");
+
+        return _factory.Wrap(data);
+    }
 }
---
+++ SuccessHound/Defaults/DefaultApiResponseFactory.cs
@@ -0,0 +1,22 @@
+﻿using SuccessHound.Abstractions;
+
+namespace SuccessHound.Defaults;
+
+/// <summary>
+/// Default factory that wraps any data type into the <see cref="ApiResponse{T}"/> envelope.
+/// Can be used out-of-the-box with SuccessHound for minimal setup.
+/// </summary>
+public class DefaultApiResponseFactory : ISuccessResponseFactory
+{
+    /// <summary>
+    /// Wraps the given data into <see cref="ApiResponse{T}"/> dynamically.
+    /// </summary>
+    /// <param name="data">The raw data to wrap.</param>
+    /// <returns>An ApiResponse{T} object containing the data.</returns>
+    public object Wrap(object data)
+    {
+        var type = typeof(ApiResponse<>).MakeGenericType(data.GetType());
+        var okMethod = type.GetMethod("Ok")!;
+        return okMethod.Invoke(null, new[] { data })!;
+    }
+}
