2d873e87915bf53fc32e21e48f0460085abb07ae
Cody <cydoentis@gmail.com>
2025-12-14 14:53:28 -0500

    feat: Added a ISuccessResponseFactory.cs with a Wrap method used to Wrap a user defined response shape

--- SuccessHound/Abstractions/ISuccessResponseFactory.cs
+++ SuccessHound/Abstractions/ISuccessResponseFactory.cs
@@ -2,6 +2,9 @@
 
 public interface ISuccessResponseFactory
 {
-    
-    
+    /// <summary>
+    /// Wraps successful data in the user-defined response shape.
+    /// </summary>
+    /// <param name="data">The raw data returned by the handler</param>
+    object Wrap(object data);
 }
